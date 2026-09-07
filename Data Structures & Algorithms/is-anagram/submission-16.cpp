4fb26d2c9add2bbcf5ee669fb1aec1a2b56d8357
Adi Dubey <adidubeys04@gmail.com>
2026-09-07 22:06:42 +0100

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.cpp
@@ -0,0 +1,24 @@
+#include <array>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        array<int, 26> sdict{};
+        array<int, 26> tdict{};
+
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            sdict[s[i] - 'a'] += 1;
+            tdict[t[i] - 'a'] += 1;
+        }
+        
+        return sdict == tdict;
+
+    }
+};
