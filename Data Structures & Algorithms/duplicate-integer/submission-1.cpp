6575f631e628bea71db9f3ee46f8b3a328fc8b97
Adi Dubey <adidubeys04@gmail.com>
2026-09-07 20:42:48 +0100

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,21 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> seen;
+
+        for (int num : nums)
+        {
+            if (seen.contains(num))
+            {
+                return true;
+            }
+
+            seen.insert(num);
+        }
+
+        return false;
+    
+    }
+};
