8ab41fd3293eaaa82e06f070bb5b5f62b9f53022
Adi Dubey <adidubeys04@gmail.com>
2026-09-10 19:16:01 +0100

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+#include <unordered_map>
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        unordered_map<char, char> dict = {{'}','{'},{']','['},{')','('}};
+        stack<char> pstack; 
+
+        for (char c : s)
+        {
+            if (dict.contains(c))
+            {
+                if ((!pstack.empty()) && pstack.top() == dict[c])
+                {
+                    pstack.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            else{
+                pstack.push(c);
+            }
+        }
+
+        return (pstack.empty());
+    }
+};
