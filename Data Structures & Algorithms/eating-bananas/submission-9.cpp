7fe1f9dd2bc201229c38da047adf7420c33ef245
Adi Dubey <adidubeys04@gmail.com>
2026-09-10 19:49:48 +0100

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int left = 1;
+        int right = *(max_element(piles.begin(), piles.end()));
+        int res = right;
+
+        while (left <= right)
+        {
+
+            int k = (left + right) / 2;
+            long long time = 0;
+            for (int p : piles)
+            {
+                time += ceil(static_cast<double>(p) / k);
+            }
+
+            if (time <= h)
+            {
+                res = k;
+                right = k - 1;
+            }
+            else
+            {
+                left = k + 1;
+            }
+
+        }
+
+        return res;
+    }
+};
